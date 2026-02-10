babd871fa0bd4123baa89ba1bd7087c2a651e878
jaytheham <jaytheham@gmail.com>
2026-02-10 16:32:00 +1300

    Names

--- src.us/overlay_gameplay/outside/cheats.c
+++ src.us/overlay_gameplay/outside/cheats.c
@@ -1,7 +1,7 @@
 #include <ultra64.h>
 #include "common.h"
 
-
+// enableCheatAnnull
 #pragma GLOBAL_ASM("asm/nonmatchings/overlay_gameplay/outside/cheats/func_800731B0_82160.s")
 
 // func_800732C8_82278
@@ -45,46 +45,56 @@ void enableCheatArsenal() {
     func_801391DC_14818C(7, 3);
 }
 
-void func_80073428_823D8() {   
+// func_80073428_823D8
+void enableCheatDurable() {   
     D_80052B34->unk1C = (u16) D_80257A00[D_80052B34->unk1A].hitPoints;
     D_80052B34->unk3C = (s16) (D_80257A00[D_80052B34->unk1A].unk61 << 8);
     func_800FD410_10C3C0(&(D_80257A00[D_80052B34->unk1A]), &D_80257A00[0]);
 }
 
+// doWanderingCheatEffect
 #pragma GLOBAL_ASM("asm/nonmatchings/overlay_gameplay/outside/cheats/func_800734AC_8245C.s")
 
-void func_8007364C_825FC(void) {
+// func_8007364C_825FC
+void enableCheatWander(void) {
     func_8007622C_851DC(func_800734AC_8245C);
 }
 
-void func_80073670_82620() {
+// func_80073670_82620
+void enableCheatSnared() {
     D_8004D148 = 0;
 }
 
-void func_8007367C_8262C() {
+// func_8007367C_8262C
+void enableCheatFreed() {
     D_8004D148 = 1;
 }
 
-void func_8007368C_8263C(void) {
+// func_8007368C_8263C
+void enableCheatAlfa(void) {
     func_800A8A68_B7A18((s16) (D_80052B34->unk0 + 0x64), (s16) (D_80052B34->unk2 - 0xFA), D_80052B34->unk4, 0x15);
     func_800A8A68_B7A18((s16) (D_80052B34->unk0 + 0x64), (s16) (D_80052B34->unk2 + 0xFA), D_80052B34->unk4, 0x17);
     func_800A8A68_B7A18((s16) (D_80052B34->unk0 - 0xC8), (s16) (D_80052B34->unk2 - 0xC8), D_80052B34->unk4, 0x18);
     func_800A8A68_B7A18((s16) (D_80052B34->unk0 - 0xC8), (s16) (D_80052B34->unk2 + 0xC8), D_80052B34->unk4, 0x19);
 }
 
-void func_8007377C_8272C() {
+// func_8007377C_8272C
+void enableCheatSnuffle() {
     D_80052ACD ^= 4;
 }
 
-void func_80073794_82744() {
+// func_80073794_82744
+void enableCheatAward() {
     D_80047FA0 = 5;
 }
 
-void func_800737A4_82754() {
+// func_800737A4_82754
+void enableCheatSurreal() {
     D_80159320 ^= 0x80;
 }
 
-void func_800737BC_8276C(void) {
+// func_800737BC_8276C
+void enableCheatDefender(void) {
     if (!(D_80052B34->unk20 & 0x80)) {
         func_800E5CF4_F4CA4(2, D_80149441);
         return;
@@ -92,43 +102,55 @@ void func_800737BC_8276C(void) {
     func_800E5E3C_F4DEC(2, D_80149441);
 }
 
-void func_80073814_827C4() {
+// func_80073814_827C4
+void enableCheatBleed() {
     func_80124118_1330C8(D_80052B34, 0x7FFF);
 }
 
+// enableCheatSuffer
 #pragma GLOBAL_ASM("asm/nonmatchings/overlay_gameplay/outside/cheats/func_8007383C_827EC.s")
 
-void func_8007391C_828CC() {
+// func_8007391C_828CC
+void enableCheatWeasel() {
     D_80052ACD ^= 2;
 }
 
+//enableCheatUseful
 #pragma GLOBAL_ASM("asm/nonmatchings/overlay_gameplay/outside/cheats/func_80073934_828E4.s")
 
-void func_800739B8_82968() {
+// func_800739B8_82968
+void enableCheatDwarf() {
     D_80157A3C = 0x68;
 }
 
-void func_800739C8_82978() {
+// func_800739C8_82978
+void enableCheatBanana() {
     D_80157A3C = 0x22;
 }
 
-void func_800739D8_82988() {
+// func_800739D8_82988
+void enableCheatDundee() {
     func_800EA2BC_F926C();
 }
 
-void func_800739F8_829A8() {
+// func_800739F8_829A8
+void enableCheatLard() {
     D_8013FCD0 = 1;
 }
 
-void func_80073A08_829B8() {
+// func_80073A08_829B8
+void enableCheatFeeble() {
     s32 x = D_8013BD04;
     D_8013BD04 = x == 0;
 }
 
+// addCharToCheatInputBuffer
 #pragma GLOBAL_ASM("asm/nonmatchings/overlay_gameplay/outside/cheats/func_80073A20_829D0.s")
 
+// initCheating
 #pragma GLOBAL_ASM("asm/nonmatchings/overlay_gameplay/outside/cheats/func_80073A74_82A24.s")
 
+// activateCheat
 #pragma GLOBAL_ASM("asm/nonmatchings/overlay_gameplay/outside/cheats/func_80073B30_82AE0.s")
 
 #pragma GLOBAL_ASM("asm/nonmatchings/overlay_gameplay/outside/cheats/func_80073B78_82B28.s")
