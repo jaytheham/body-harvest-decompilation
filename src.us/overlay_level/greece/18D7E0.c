8dbee84845adb2dbbdb378523b5ffd17705d1cdc
Harvester <harvester@lorben.uk>
2026-03-09 15:23:06 +0000

    decomp: greece/18D7E0 — NON_MATCHING bodies for func_802D64D0 and func_802D6904
    Add C bodies for the two remaining stubs in the Greece zone:
    - func_802D64D0_18EFE0: alien spawn + state machine (9-case switch)
    - func_802D6904_18F414: alien approach sequence (9-case switch, float init)
    
    Both functions are dispatch-based state machines registered via
    func_800EFEB4_FEE64. Jump tables extracted from ROM at 0x196E84 / 0x196EA8.
    Kept NON_MATCHING pending diff verification and struct field reconciliation
    with upstream AlienSpec changes.

--- src.us/overlay_level/greece/18D7E0.c
+++ src.us/overlay_level/greece/18D7E0.c
@@ -514,9 +514,240 @@ block_12:
 
 
 
-#pragma GLOBAL_ASM("asm/nonmatchings/overlay_level/greece/18D7E0/func_802D64D0_18EFE0.s")
+#ifdef NON_MATCHING
+s32 func_802D64D0_18EFE0(void) {
+	s16 step;
+	s16 substep;
+	s16 alienIdx;
+	s32 v0;
+	AlienInstance *alien;
+	BuildingInstance *building;
 
+	step = D_80157F94;
+	D_80157F96 = 0;
+
+	if (step != (s16) 0xFF) {
+		alienIdx = step * 5;
+		func_800800E4_8F094(*(u8 *) ((u8 *) &D_800481B2 + alienIdx * 16));
+		func_80080510_8F4C0(D_80157F95);
+		step = D_80157F94;
+		alien = &alienInstances[step];
+		alien->unk6 = alien->unkE;
+		((void (*)(u8)) *(s32 *) ((u8 *) &D_802566C8 + ((alienIdx * 4 - alienIdx) * 4 + alienIdx) * 8))((u8) step);
+	}
+
+	substep = D_80157F8E;
+	switch (D_80157F8C) {
+	case 0:
+		func_8011D260_12C210(0x4A, -0x7);
+		D_80159DDF = (u8) v0;
+		D_80159DE0 = 0;
+		D_80159DE2 = 0;
+		D_80159DE4 = 0;
+		D_80157F8E = 0;
+		D_80052554 = 0x400;
+		D_80157F98 = func_800CDB40_DCAF0(0x4AF0, 0x3B6, -0x664);
+		D_80157F9A = func_800CDB40_DCAF0(0x4AFC, 0x3B6, -0x69C);
+		func_800072CC_7ECC(0, 0x2A);
+		D_80157F8C++;
+		v0 = func_8007956C_8851C(0x12);
+		alien = &alienInstances[v0];
+		building = buildingInstances;
+		alien->unkE = alien->unkE | 0x100;
+		alien->unk0 = building->unk918 + 0x80;
+		alien->unk14 = building->unk918 + 0x600;
+		alien->unkE = alien->unkE & 0xF7FFFFFF;
+		alien->unk24 = 2;
+		alien->unk4 = building->unk91C;
+		alien->unk18 = building->unk91C;
+		func_8007A198_89148(v0);
+		D_80157F94 = (s16) v0;
+		break;
+	case 1:
+		if (substep >= 0xD8) {
+			D_80157F8E = 0;
+			D_80157F8C++;
+		} else {
+			D_80157F8E = substep + 1;
+		}
+		break;
+	case 2:
+		func_800E35E0_F2590(0xA0);
+		D_80157F8C++;
+		break;
+	case 3:
+		func_800E35E0_F2590(0xFF);
+		D_80157F8C++;
+		break;
+	case 4:
+		func_800E35E0_F2590(0xA0);
+		D_80157F8C++;
+		break;
+	case 5:
+		func_800CDD7C_DCD2C(D_80157F98);
+		func_800CDD7C_DCD2C(D_80157F9A);
+		func_800DFBA8_EEB58(0x4AFC, 0x3A2, -0x680, 0xB4, 6);
+		func_800C7E18_D6DC8();
+		func_800CA5EC_D959C(0x4AFC, 0x3A2, -0x680, 0x28, 0x32, 0xFF, 0x28, 0x32, 0xA, 0x19, 0xB4, 0x5A, 0x50, 0x2D, 0xFF, 0x32);
+		func_800CA5EC_D959C(0x4AFC, 0x3A2, -0x680, -0x28, 0x32, 0xFF, -0x28, 0x32, 0xA, 0x19, 0xB4, 0x5A, 0x50, 0x2D, 0xFF, 0x32);
+		func_800E35E0_F2590(0x50);
+		D_80157F8C++;
+		break;
+	case 6:
+		if (substep >= 0x33) {
+			D_80157F8E = 0;
+			D_80157F8C++;
+		} else {
+			D_80157F8E = substep + 1;
+		}
+		break;
+	case 7:
+		if (substep >= 0x2) {
+			D_80157F8E = 0;
+			D_80157F8C++;
+		} else {
+			D_80157F8E = substep + 1;
+		}
+		break;
+	case 8:
+		func_800072CC_7ECC(0, 0x28);
+		func_800073B8_7FB8(0, 0x2A);
+		D_80159DDF = 0xFF;
+		func_80079910_888C0(D_80157F94);
+		return 1;
+	}
+	return 0;
+}
+#else
+#pragma GLOBAL_ASM("asm/nonmatchings/overlay_level/greece/18D7E0/func_802D64D0_18EFE0.s")
+#endif
+
+#ifdef NON_MATCHING
+s32 func_802D6904_18F414(void) {
+	s16 step;
+	s16 substep;
+	s16 alienIdx;
+	s32 v0;
+	AlienInstance *alien;
+
+	D_80157F96 = 0;
+	D_80157F76 = 0x12C;
+	D_80157FAC = 0xE;
+	D_80157FAE = 0xB;
+	D_80157FB0 = -0x9;
+
+	osSyncPrintf(D_802DE30C, D_80157F8C, D_80157F8E);
+
+	alienIdx = D_80157F94;
+	if (alienIdx != (s16) 0xFF) {
+		s32 idx5 = alienIdx * 5;
+		if (*(u8 *) ((u8 *) &D_800481B2 + idx5 * 16) != 0) {
+			func_800800E4_8F094((u8) alienIdx);
+			func_80080510_8F4C0(D_80157F95);
+			alienIdx = D_80157F94;
+			alien = &alienInstances[alienIdx];
+			alien->unk6 = alien->unkE;
+			idx5 = alien->unk1A;
+			((void (*)(u8)) *(s32 *) ((u8 *) &D_802566C8 + ((idx5 * 4 - idx5) * 4 + idx5) * 8))((u8) alienIdx);
+		} else {
+			D_80157F94 = 0xFF;
+		}
+	}
+
+	substep = D_80157F8E;
+	switch (D_80157F8C) {
+	case 0:
+		D_80157F8E = 0;
+		D_80157F8C++;
+		D_80157F84 = 200.0f;
+		D_80157F88 = 0.0f;
+		D_80157F78 = 400.0f;
+		D_80157F7C = -2.0f;
+		D_80157F80 = 100.0f;
+		v0 = func_8007956C_8851C(0x12);
+		if (v0 != (s16) 0xFF) {
+			alien = &alienInstances[v0];
+			alien->unkE = alien->unkE & 0xF7FFFE5F;
+			alien->unk0 = 0x4C80;
+			alien->unk4 = -0x6780;
+			alien->unk14 = 0x4C80;
+			alien->unk18 = -0x6380;
+			alien->unkE = alien->unkE | 0x100;
+			alien->unk24 = 2;
+			alien->unkE = alien->unkE;
+			alien->unk2A = 0x4000;
+			func_8007A198_89148(v0);
+			alien->unkE = alien->unkE;
+			D_80157F94 = (s16) v0;
+		}
+		alienIdx = D_80157F94;
+		alien = &alienInstances[alienIdx];
+		D_80157F98 = func_800CDB40_DCAF0(alien->unk0, alien->unk2, alien->unk4);
+		break;
+	case 1:
+		if (substep >= 0x38) {
+			D_80157F8E = 0;
+			D_80157F8C++;
+		} else {
+			D_80157F8E = substep + 1;
+		}
+		break;
+	case 2:
+		func_800CDD7C_DCD2C(D_80157F98);
+		func_800DFBA8_EEB58(0x4C7C, 0x350, -0x675C, 0xB4, 6);
+		D_80157F8C++;
+		break;
+	case 3:
+		if (substep >= 0x5) {
+			D_80157F8E = 0;
+			D_80157F8C++;
+		} else {
+			D_80157F8E = substep + 1;
+		}
+		break;
+	case 4:
+		func_80124B5C_133B0C(0x4C7C, 0x350, -0x675C, 0x2711, 0x300);
+		D_80157F8C++;
+		break;
+	case 5:
+		if (substep >= 0x29) {
+			D_80157F8E = 0;
+			D_80157F8C++;
+		} else {
+			D_80157F8E = substep + 1;
+		}
+		break;
+	case 6:
+		alienIdx = D_80157F94;
+		alien = &alienInstances[alienIdx];
+		osSyncPrintf(D_802DE318, alien->unk0 + 0x96, alien->unk2, alien->unk4);
+		func_80087AAC_96A5C(D_80157F95);
+		alienIdx = D_80157F94;
+		((u8 *) &D_800481C4)[alienIdx * 5 * 16] = 0x14;
+		D_80157F8C++;
+		break;
+	case 7:
+		D_80157F94 = alienIdx;
+		if (alienIdx != (s16) 0xFF) {
+			func_80087AFC_96AAC((u8) alienIdx);
+		}
+		substep = D_80157F8E;
+		if (substep >= 0x1F) {
+			D_80157F8E = 0;
+			D_80157F8C++;
+		} else {
+			D_80157F8E = substep + 1;
+		}
+		break;
+	case 8:
+		D_80159DDF = 0xFF;
+		return 1;
+	}
+	return 0;
+}
+#else
 #pragma GLOBAL_ASM("asm/nonmatchings/overlay_level/greece/18D7E0/func_802D6904_18F414.s")
+#endif
 
 void func_802D6D20_18F830(void) {
 	u8 selector;
