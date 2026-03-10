010200491067c67101eab845d7da49d117677437
Harvester <harvester@lorben.uk>
2026-03-10 23:59:25 +0000

    reclaim: 5 functions in comet/318E20 [MATCHING] su-20 through su-24

--- include/functions.us.h
+++ include/functions.us.h
@@ -559,5 +559,8 @@ s32 func_800B960C_C85BC(s16 arg0, s16 arg1, s16 arg2, s16 arg3);
 void func_802D522C_31937C(s16 arg0);
 void func_802D67A8_31A8F8(void);
 void func_800C541C_D43CC(s16 arg0, s16 arg1, s16 arg2, s32 arg3, s32 arg4, s32 arg5, s32 arg6, s32 arg7, s32 arg8, s32 arg9, s8 arg10, s32 arg11, s32 arg12);
+s32 func_802DFF04_324054(u8 arg0);
+void func_802D6670_31A7C0(void);
+void func_802D5BFC_319D4C(void);
 
 #endif
