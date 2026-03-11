9b39cbf8ea1aff70fb21916421ca62bc75a4bc44
Harvester <harvester@lorben.uk>
2026-03-11 12:09:25 +0000

    reclaim: func_802E46B8_328808 in comet/318E20 [MATCHING]
    Alien slot reassignment — transfers unk25 chain, allocates new slot
    via func_8007956C, stores ptr in D_80140AB0[arg1], raw byte offset
    write via ((u8*)&inst + arg1 - 1) pattern.
    
    New vars: D_80140AB0[], D_802E7AE4 (variables.us.h)

--- src.us/overlay_level/comet/318E20.c
+++ src.us/overlay_level/comet/318E20.c
@@ -728,7 +728,19 @@ void func_802E3E30_327F80(u8 arg0) {
 
 #pragma GLOBAL_ASM("asm/nonmatchings/overlay_level/comet/318E20/func_802E43FC_32854C.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/overlay_level/comet/318E20/func_802E46B8_328808.s")
+void func_802E46B8_328808(u8 arg0, s32 arg1, u8 arg2) {
+    u8 old_unk25;
+    u8 new_idx;
+    AlienInstance *new_inst;
+
+    old_unk25 = alienInstances[arg0].unk25;
+    new_idx = func_8007956C_8851C(arg0);
+    new_inst = &alienInstances[new_idx];
+    new_inst->unk25 = old_unk25;
+    *((u8 *)&alienInstances[alienInstances[old_unk25].unk25] + arg1 - 1) = new_idx;
+    D_80140AB0[arg1] = new_inst;
+    osSyncPrintf(D_802E7AE4, new_idx, arg2);
+}
 
 #pragma GLOBAL_ASM("asm/nonmatchings/overlay_level/comet/318E20/func_802E4774_3288C4.s")
 
