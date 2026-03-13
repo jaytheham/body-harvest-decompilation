b1fd4532fa45ae58b152a934c2e1e693d1ac3fcf
Harvester <harvester@lorben.uk>
2026-03-13 19:58:47 +0000

    reclaim: func_8000FFC0_10BC0 in core/loader [MATCHING]

--- src.us/core/loader.c
+++ src.us/core/loader.c
@@ -1,8 +1,85 @@
 #include <ultra64.h>
 #include "common.h"
 
+extern s16 D_80068078;
+extern s32 D_8006AA60;
+extern s32 D_8006AA64;
+extern char D_80037780[];
+extern char D_80037794[];
+extern char D_800377A8[];
+extern char D_800377BC[];
+extern char D_800377D0[];
+extern char D_800377E4[];
+extern char D_800377F8[];
 
-#pragma GLOBAL_ASM("asm/nonmatchings/core/loader/func_8000FFC0_10BC0.s")
+s32 func_8000FFC0_10BC0(s32 arg0, s32 arg1, s32 arg2, s32 arg3) {
+    s32 var_s0;
+    s32 var_s1;
+    s32 var_s2;
+    s32 var_v1;
+    s32 temp_t7;
+    s32 temp_v0;
+    s32 temp_v0_2;
+    s32 temp_v0_3;
+    s32 sp50;
+    s32 sp58;
+
+    if (D_80068078 == 1) {
+        return 0;
+    }
+    func_8001F2E0_1FEE0();
+    var_s1 = arg1;
+    var_s2 = arg2;
+    temp_t7 = arg3 / 2048;
+    var_v1 = temp_t7;
+    var_s0 = temp_t7 - 1;
+    if (temp_t7 != 0) {
+        sp50 = temp_t7;
+        do {
+            osPiStartDma(&sp58, 0, 0, var_s2, var_s1, 0x800, arg0);
+            osRecvMesg(arg0, 0, 1);
+            temp_v0 = func_8001F6E0_202E0();
+            switch (temp_v0) {
+            case 2:
+                osSyncPrintf(D_80037780);
+                break;
+            case 1:
+                osSyncPrintf(D_80037794);
+                break;
+            case 4:
+                osSyncPrintf(D_800377A8);
+                break;
+            }
+            var_s2 += 0x800;
+            var_s1 += 0x800;
+            var_s0 -= 1;
+        } while (var_s0 != 0);
+        var_v1 = sp50;
+    }
+    temp_v0_2 = arg3 - (var_v1 << 0xB);
+    if (temp_v0_2 != 0) {
+        osPiStartDma(&sp58, 0, 0, var_s2, var_s1, temp_v0_2, arg0);
+        osRecvMesg(arg0, 0, 1);
+        temp_v0_3 = func_8001F6E0_202E0();
+        switch (temp_v0_3) {
+        case 2:
+            osSyncPrintf(D_800377BC);
+            break;
+        case 1:
+            osSyncPrintf(D_800377D0);
+            break;
+        case 4:
+            osSyncPrintf(D_800377E4);
+            break;
+        }
+    }
+    arg2 += arg3;
+    if (D_8006AA64 == 0xBABEFACE) {
+        osSyncPrintf(D_800377F8, arg3);
+    }
+    D_8006AA60 = arg3;
+    return arg2;
+}
 
 #pragma GLOBAL_ASM("asm/nonmatchings/core/loader/func_800101F0_10DF0.s")
 
