2a3f7d94593c89e19d633ac350a13713fb237e42
Meridian Lex <259898603+Meridian-Lex@users.noreply.github.com>
2026-03-14 22:30:51 +0000

    reclaim: func_8000EF10_FB10 in core/FB00 [MATCHING]

--- src.us/core/FB00.c
+++ src.us/core/FB00.c
@@ -1,10 +1,23 @@
 #include <ultra64.h>
 #include "common.h"
 
+extern s16 D_80068078;
+extern u8 D_8003FB20[];
+extern OSThread D_80067388;
+extern void *D_8005C760;
+extern void func_8000EFB8_FBB8(void *);
 
 #pragma GLOBAL_ASM("asm/nonmatchings/core/FB00/sourceTaggedPrintF.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/core/FB00/func_8000EF10_FB10.s")
+void func_8000EF10_FB10(s32 arg0) {
+    D_80068078 = 0;
+    bzero(&D_8003FB20, 0x803FFFFF - (s32)(&D_8003FB20));
+    osInitialize();
+    D_80067388.next = NULL;
+    D_80067388.queue = NULL;
+    osCreateThread(&D_80067388, 1, func_8000EFB8_FBB8, NULL, &D_8005C760, 0xA);
+    osStartThread(&D_80067388);
+}
 
 #pragma GLOBAL_ASM("asm/nonmatchings/core/FB00/func_8000EF98_FB98.s")
 
