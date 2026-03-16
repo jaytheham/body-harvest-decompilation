c063197c0bb57d6ea64b0825b76c97e342972dd7
Meridian Lex <miridian.lex@lorben.uk>
2026-03-16 00:26:36 +0000

    reclaim: func_802D5FBC_1EECCC in java/1ED9E0 [MATCHING]

--- src.us/overlay_level/java/1ED9E0.c
+++ src.us/overlay_level/java/1ED9E0.c
@@ -445,9 +445,17 @@ block_13:
 #pragma GLOBAL_ASM("asm/nonmatchings/overlay_level/java/1ED9E0/func_802D5E98_1EEBA8.s")
 #endif
 
+#ifdef NON_MATCHING
+void func_802D5F98_1EECA8(void) {
+    func_80007410_8010(&func_802D5E98_1EEBA8);
+}
+#else
 #pragma GLOBAL_ASM("asm/nonmatchings/overlay_level/java/1ED9E0/func_802D5F98_1EECA8.s")
+#endif
 
-#pragma GLOBAL_ASM("asm/nonmatchings/overlay_level/java/1ED9E0/func_802D5FBC_1EECCC.s")
+void func_802D5FBC_1EECCC(void) {
+    func_800EFEB4_FEE64(0, 0xF, 0);
+}
 
 #pragma GLOBAL_ASM("asm/nonmatchings/overlay_level/java/1ED9E0/func_802D5FE4_1EECF4.s")
 
