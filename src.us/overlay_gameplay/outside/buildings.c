9870b24b735dfd81dffbca7af9cb8f7ff769a44b
jaytheham <jaytheham@gmail.com>
2026-03-20 10:18:07 +1300

    Match - sonnet 4.6

--- src.us/overlay_gameplay/outside/buildings.c
+++ src.us/overlay_gameplay/outside/buildings.c
@@ -240,7 +240,16 @@ s32 func_801185F8_1275A8(BuildingInstance *arg0, s16 arg1) {
 
 #pragma GLOBAL_ASM("asm/nonmatchings/overlay_gameplay/outside/buildings/func_8011A604_1295B4.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/overlay_gameplay/outside/buildings/func_8011B3F0_12A3A0.s")
+void func_8011B3F0_12A3A0(s16 arg0, s16 *arg1, s16 *arg2, s16 *arg3) {
+	s16 sp26;
+	s32 flag;
+
+	flag = 0;
+	if (func_80118114_1270C4(arg0) != 0) {
+		flag = 1;
+	}
+	func_801165FC_1255AC((u8) arg0, (u8) flag, arg1, arg2, arg3, &sp26);
+}
 
 #pragma GLOBAL_ASM("asm/nonmatchings/overlay_gameplay/outside/buildings/func_8011B454_12A404.s")
 
