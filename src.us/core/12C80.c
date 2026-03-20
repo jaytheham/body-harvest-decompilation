6bc12c904d3cc307723bc0dd129603a7f0403d65
Meridian Lex <miridian.lex@lorben.uk>
2026-03-20 23:36:31 +0000

    reclaim: func_80015380_15F80 in core/12C80 [MATCHING]

--- src.us/core/12C80.c
+++ src.us/core/12C80.c
@@ -118,7 +118,8 @@
 
 #pragma GLOBAL_ASM("asm/nonmatchings/core/12C80/func_80015210_15E10.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/core/12C80/func_80015380_15F80.s")
+void func_80015380_15F80(s8 arg0) {
+}
 
 #pragma GLOBAL_ASM("asm/nonmatchings/core/12C80/func_80015388_15F88.s")
 
