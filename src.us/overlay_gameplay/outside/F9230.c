872f6b3677b2e1b656a1bd9d1f7a84eb192b54d7
jaytheham <jaytheham@gmail.com>
2026-03-21 15:58:47 +1300

    Match - raptor mini

--- src.us/overlay_gameplay/outside/F9230.c
+++ src.us/overlay_gameplay/outside/F9230.c
@@ -495,7 +495,12 @@ f32 func_800FB014_109FC4(VehicleInstance *arg0) {
 	return (f32)(((f64)(f32)coss(arg0->unk10) / 32768.0) * ((f64)(f32)sp1E / 32768.0) * (f64)arg0->unk58 + (f64)arg0->unk30);
 }
 
-#pragma GLOBAL_ASM("asm/nonmatchings/overlay_gameplay/outside/F9230/func_800FB098_10A048.s")
+f32 func_800FB098_10A048(VehicleInstance *arg0) {
+	s16 sp1E;
+
+	sp1E = sins(arg0->unkE);
+	return (f32)(((f64)(f32)coss(arg0->unk10) / 32768.0) * ((f64)(f32)sp1E / 32768.0) * (f64)arg0->unk58 + (f64)arg0->unk38);
+}
 
 f32 func_800FB11C_10A0CC(VehicleInstance *arg0) {
 	f32 sp1C = func_800FB014_109FC4(arg0);
