88a89e420afcb9c258941d256a5f5c29e3b1b636
jaytheham <jaytheham@gmail.com>
2026-03-23 12:07:36 +1300

    Non-matching decomp of func_800EA088_F9038

--- src.us/overlay_gameplay/outside/F7870.c
+++ src.us/overlay_gameplay/outside/F7870.c
@@ -19,7 +19,38 @@ s16 func_800E9868_F8818(s32 arg0, s32 arg1) {
 #pragma GLOBAL_ASM("asm/nonmatchings/overlay_gameplay/outside/F7870/func_800E988C_F883C.s")
 
 // StopDrawingShadows ?
+#ifdef NON_MATCHING
+void func_800EA088_F9038(void) {
+    Gfx *temp_v1;
+    Gfx *temp_v1_2;
+    Gfx *temp_v1_3;
+    Gfx *temp_v1_4;
+    Gfx *temp_v1_5;
+
+    temp_v1 = D_8005BB2C;
+    D_8005BB2C = temp_v1 + 1;
+    temp_v1->words.w1 = 0;
+    temp_v1->words.w0 = 0xE7000000;
+    temp_v1_2 = D_8005BB2C;
+    D_8005BB2C = temp_v1_2 + 1;
+    temp_v1_2->words.w1 = 0;
+    temp_v1_2->words.w0 = 0xE8000000;
+    temp_v1_3 = D_8005BB2C;
+    D_8005BB2C = temp_v1_3 + 1;
+    temp_v1_3->words.w1 = 0x80008000;
+    temp_v1_3->words.w0 = 0xBB000001;
+    temp_v1_4 = D_8005BB2C;
+    D_8005BB2C = temp_v1_4 + 1;
+    temp_v1_4->words.w1 = 0xC8112078;
+    temp_v1_4->words.w0 = 0xB900031D;
+    temp_v1_5 = D_8005BB2C;
+    D_8005BB2C = temp_v1_5 + 1;
+    temp_v1_5->words.w0 = 0xB7000000;
+    temp_v1_5->words.w1 = 0x20001;
+}
+#else
 #pragma GLOBAL_ASM("asm/nonmatchings/overlay_gameplay/outside/F7870/func_800EA088_F9038.s")
+#endif
 
 // DisplayVehicleShadows
 #pragma GLOBAL_ASM("asm/nonmatchings/overlay_gameplay/outside/F7870/func_800EA12C_F90DC.s")
