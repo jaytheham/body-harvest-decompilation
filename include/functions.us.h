9bd5b42dbdd718ae352fb29cc53fbd4c8c42dbf2
jaytheham <jaytheham@gmail.com>
2026-03-23 17:08:03 +1300

    Add missing

--- include/functions.us.h
+++ include/functions.us.h
@@ -270,9 +270,11 @@ void func_800D6084_E5034(u8 arg0, s16 arg1, s16 arg2, s16 arg3, u8 arg4, u8 arg5
 void func_800B0C80_BFC30(void);
 s32 func_800B0D10_BFCC0(s32, s32, s32);
 s16 func_800B0DF4_BFDA4(s16, s16, s32, s32);
+void func_80005654_6254(u8, u8, u8);
 void func_80015C94_16894(s32, s32);
 void func_80018D14_19914();
 s16 func_800B0F20_BFED0(s16, s16);
+void func_800078E4_84E4(s32 arg0, s32 *arg1);
 void func_800B31FC_C21AC(s8, s8);
 s32 func_800B325C_C220C(s8, s8, u16);
 void func_800B41C8_C3178(s32, s32, u8*, s32);
