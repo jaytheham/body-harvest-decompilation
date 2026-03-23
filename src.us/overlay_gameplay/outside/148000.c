34bde6d2102c5652477f83c3bdb14c6de73aa39a
jaytheham <jaytheham@gmail.com>
2026-03-24 00:58:57 +1300

    Non-matching decomp of func_801394DC_14848C

--- src.us/overlay_gameplay/outside/148000.c
+++ src.us/overlay_gameplay/outside/148000.c
@@ -89,7 +89,30 @@ void func_80139460_148410(void) {
 	D_801601D8 = -1;
 }
 
+// https://decomp.me/scratch/TODO
+// Score 210: D_801601E8 &= ~bit generates lui at; sw imm(at) instead of sw 0(s2).
+// Compound &= produces correct register ordering (nor t0 before lw t9) but wrong store address.
+#ifdef NON_MATCHING
+void func_801394DC_14848C(void) {
+    s32 bit;
+    s32 i;
+
+    for (i = 2; i != 0xB; i++) {
+        bit = 1 << i;
+        if (bit & D_801601E8) {
+            func_801391DC_14818C(i, 0);
+            D_801601E8 &= ~bit;
+        }
+    }
+    if (currentLevel == LEVEL_COMET) {
+        D_80048140[3] = 0x64;
+        D_80048140[4] = 0x190;
+        D_80048140[6] = 0x18;
+    }
+}
+#else
 #pragma GLOBAL_ASM("asm/nonmatchings/overlay_gameplay/outside/148000/func_801394DC_14848C.s")
+#endif
 
 #pragma GLOBAL_ASM("asm/nonmatchings/overlay_gameplay/outside/148000/func_8013958C_14853C.s")
 
