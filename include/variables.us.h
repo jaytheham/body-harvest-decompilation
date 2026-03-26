250e174f41e861aa41bcd8afe71129b1bbc76fda
jaytheham <jaytheham@gmail.com>
2026-03-26 16:39:20 +1300

    Remove dupes

--- include/variables.us.h
+++ include/variables.us.h
@@ -499,8 +499,6 @@ extern u8 D_80140D90[];
 extern u8 D_80140DA8[];
 extern u8 D_80140DC0[];
 extern f32 D_801411A0;
-extern Unk80152D00Pair D_80152D00[]; /* laser timer array (32 pairs) */
-extern Unk80152D00Pair D_80153300; /* one past end of D_80152D00 */
 extern s32 D_801416D0_150680[];
 extern s32 D_80141734_1506E4[];
 extern f64 D_801426C0;
@@ -642,9 +640,7 @@ extern u8 D_8014FDD0[];
 extern u8 D_80151DD0;
 extern Unk80152B80 D_80152B80[15]; /* particle/effect array */
 extern Unk80152D00 D_80152D00[];
-extern s8 D_80153300;
 extern u8 D_80152B88; /* unk8 field of first particle/effect entry */
-extern Unk80152D00Pair D_80152D00[]; /* laser timer array (32 pairs) */
 extern Unk80152D00Pair D_80153300; /* one past end of D_80152D00 */
 extern s16 D_80152C90;
 extern u8 D_80152740[];
