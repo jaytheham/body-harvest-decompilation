265b923a824c35e977df721d83a3317e689850b3
Meridian Lex <miridian.lex@lorben.uk>
2026-03-26 03:27:56 +0000

    reclaim: func_802D5578_18E088 in greece/18D7E0 [MATCHING]

--- src.us/overlay_level/greece/18D7E0.c
+++ src.us/overlay_level/greece/18D7E0.c
@@ -119,14 +119,13 @@ void func_802D54AC_18DFBC(void) {
 	func_800076D4_82D4(5);
 }
 
-#ifdef NON_MATCHING
 void func_802D5578_18E088(void) {
 	u16 temp_t3;
-	if (!(vehicleInstances[84].unk20 & 0x8000)) {
+	if (!(D_8004FB00.unk20 & 0x8000)) {
 		func_800074BC_80BC(func_802D5578_18E088);
 	}
-	if ((vehicleInstances[84].unk0 < 0x1000) && (vehicleInstances[84].unk4 < -0x6B00)) {
-		vehicleInstances[84].unk20 = (u16)(vehicleInstances[84].unk20 & 0xFBFF);
+	if ((D_8004FB00.unk0 < 0x1000) && (D_8004FB00.unk4 < -0x6B00)) {
+		D_8004FB00.unk20 = (u16)(D_8004FB00.unk20 & 0xFBFF);
 		func_800074BC_80BC(func_802D5578_18E088);
 	}
 	temp_t3 = D_802DE466 + 1;
@@ -135,9 +134,6 @@ void func_802D5578_18E088(void) {
 		func_80007690_8290();
 	}
 }
-#else
-#pragma GLOBAL_ASM("asm/nonmatchings/overlay_level/greece/18D7E0/func_802D5578_18E088.s")
-#endif
 
 void func_802D562C_18E13C(void) {
 	func_80092ADC_A1A8C();
