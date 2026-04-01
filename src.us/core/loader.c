f25225ebab07eb186652ecde63d1f745ee50ccb1
jaytheham <jaytheham@gmail.com>
2026-04-02 10:36:06 +1300

    Decompile func_800101F0_10DF0

--- src.us/core/loader.c
+++ src.us/core/loader.c
@@ -1,6 +1,8 @@
 #include <ultra64.h>
 #include "common.h"
 
+s32 func_8000FFC0_10BC0(s32 arg0, s32 arg1, s32 arg2, s32 arg3);
+
 #ifdef NON_MATCHING
 s32 func_8000FFC0_10BC0(s32 arg0, s32 arg1, s32 arg2, s32 arg3) {
 	s32 var_s0;
@@ -74,7 +76,9 @@ s32 func_8000FFC0_10BC0(s32 arg0, s32 arg1, s32 arg2, s32 arg3) {
 #pragma GLOBAL_ASM("asm/nonmatchings/core/loader/func_8000FFC0_10BC0.s")
 #endif
 
-#pragma GLOBAL_ASM("asm/nonmatchings/core/loader/func_800101F0_10DF0.s")
+s32 func_800101F0_10DF0(s32 arg0, s32 arg1, s32 arg2) {
+	return func_8000FFC0_10BC0(&D_80067F70, arg0, arg1, arg2);
+}
 
 void func_80010228_10E28(u32 rom_addr, void *dest_buffer) {
 	OSIoMesg io_msg;
