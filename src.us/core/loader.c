cdde389cc16207f43f55013bdd801664274205bb
jaytheham <jaytheham@gmail.com>
2026-04-09 18:44:32 +1200

    Match

--- src.us/core/loader.c
+++ src.us/core/loader.c
@@ -207,54 +207,54 @@ s32 func_800104AC_110AC(u8 arg0) {
 
 #ifdef NON_MATCHING
 s32 func_800105F0_111F0(s32 *arg0, s32 *arg1, s32 *arg2, s32 arg3, u8 arg4, s32 *arg5) {
-    s32 sp34;
-    s32 sp30;
-    s32 sp2C;
-    s32 *sp28;
-    s32 var_v0;
-    s32 var_a1;
-    s32 var_a1_2;
-    s32 var_s0;
-    s32 var_s1;
+	s32 sp34;
+	s32 sp30;
+	s32 sp2C;
+	s32 *sp28;
+	s32 var_v0;
+	s32 var_a1;
+	s32 var_a1_2;
+	s32 var_s0;
+	s32 var_s1;
 
-    var_v0 = func_800101F0_10DF0((s32)arg0, func_800104AC_110AC(arg4), 0xFE00);
-    var_a1 = var_v0;
-    if (var_v0 & 0xF) {
-        var_a1 = (var_v0 - (var_v0 & 0xF)) + 0x10;
-    }
+	var_v0 = func_800101F0_10DF0((s32)arg0, func_800104AC_110AC(arg4), 0xFE00);
+	var_a1 = var_v0;
+	if (var_v0 & 0xF) {
+		var_a1 = (var_v0 - (var_v0 & 0xF)) + 0x10;
+	}
 
-    var_v0 = func_800101F0_10DF0((s32)sp28, var_a1, 4);
+	var_v0 = func_800101F0_10DF0((s32)sp28, var_a1, 4);
 
-    switch (D_800313D0) {
-    default:
-    case 0:
-        var_a1_2 = func_800101F0_10DF0((s32)arg5, func_800101F0_10DF0((s32)arg1, var_v0, 0x3800), 0x100) + 0x7200;
-        break;
-    case 1:
-        var_v0 += 0x3900;
-        var_a1_2 = func_800101F0_10DF0((s32)arg5, func_800101F0_10DF0((s32)arg1, var_v0, 0x3800), 0x100) + 0x3900;
-        break;
-    case 2:
-        var_v0 += 0x7200;
-        var_a1_2 = func_800101F0_10DF0((s32)arg5, func_800101F0_10DF0((s32)arg1, var_v0, 0x3800), 0x100);
-        break;
-    }
+	switch (D_800313D0) {
+	default:
+	case 0:
+		var_a1_2 = func_800101F0_10DF0((s32)arg5, func_800101F0_10DF0((s32)arg1, var_v0, 0x3800), 0x100) + 0x7200;
+		break;
+	case 1:
+		var_v0 += 0x3900;
+		var_a1_2 = func_800101F0_10DF0((s32)arg5, func_800101F0_10DF0((s32)arg1, var_v0, 0x3800), 0x100) + 0x3900;
+		break;
+	case 2:
+		var_v0 += 0x7200;
+		var_a1_2 = func_800101F0_10DF0((s32)arg5, func_800101F0_10DF0((s32)arg1, var_v0, 0x3800), 0x100);
+		break;
+	}
 
-    var_v0 = func_800101F0_10DF0((s32)arg2, var_a1_2, 0x100);
-    var_s0 = arg3;
-    var_s1 = 0;
-    do {
-        if (var_v0 & 0xF) {
-            var_v0 = (var_v0 - (var_v0 & 0xF)) + 0x10;
-        }
-        var_v0 = func_8001032C_10F2C(var_s0, var_v0, (s32)&D_803DA800);
-        var_s0 += 0x1400;
-        var_v0 = func_8001032C_10F2C(var_s0, var_v0, (s32)&D_803DA800);
-        var_s1 = (var_s1 + 1) & 0xFF;
-        var_s0 += 0x200;
-    } while (var_s1 < 0xC);
+	var_v0 = func_800101F0_10DF0((s32)arg2, var_a1_2, 0x100);
+	var_s0 = arg3;
+	var_s1 = 0;
+	do {
+		if (var_v0 & 0xF) {
+			var_v0 = (var_v0 - (var_v0 & 0xF)) + 0x10;
+		}
+		var_v0 = func_8001032C_10F2C(var_s0, var_v0, (s32)&D_803DA800);
+		var_s0 += 0x1400;
+		var_v0 = func_8001032C_10F2C(var_s0, var_v0, (s32)&D_803DA800);
+		var_s1 = (var_s1 + 1) & 0xFF;
+		var_s0 += 0x200;
+	} while (var_s1 < 0xC);
 
-    return *sp28;
+	return *sp28;
 }
 #else
 #pragma GLOBAL_ASM("asm/nonmatchings/core/loader/func_800105F0_111F0.s")
@@ -387,14 +387,12 @@ s32 func_800119A8_125A8(s32 arg0)
   return &D_500A00 - &D_4EBF80 + arg0;
 }
 
-#ifdef NON_MATCHING
-s32 func_800119F4_125F4(s32 arg0) {
-	func_800101F0_10DF0(D_8006AA6C = arg0, &D_500A00, &D_55B0A0 - &D_500A00);
-	return (&D_55B0A0 - &D_500A00) + arg0;
+s32 func_800119F4_125F4(s32 arg0)
+{
+  D_8006AA6C = arg0;
+  func_800101F0_10DF0(D_8006AA6C, &D_500A00, &D_55B0A0 - &D_500A00);
+  return &D_55B0A0 - &D_500A00 + arg0;
 }
-#else
-#pragma GLOBAL_ASM("asm/nonmatchings/core/loader/func_800119F4_125F4.s")
-#endif
 
 #ifdef NON_MATCHING
 s32 func_80011A40_12640(u8 arg0, s32 arg1) {
@@ -497,15 +495,15 @@ s32 func_80011DBC_129BC(s32 arg0) {
 }
 
 void func_80011E14_12A14(u8 arg0) {
-    gSPSegment(D_8005BB2C++, 0, 0);
-    gSPSegment(D_8005BB2C++, 1, (u32)__printfunc & 0x1FFFFFFF);
-    gSPSegment(D_8005BB2C++, 5, (u32)D_8006AA6C & 0x1FFFFFFF);
-    gSPSegment(D_8005BB2C++, 0xF, (u32)D_8006AA74 & 0x1FFFFFFF);
-    gSPSegment(D_8005BB2C++, D_80031C68_32868[arg0], (u32)D_8006AA70 & 0x1FFFFFFF);
-    if (D_80052ACA == 2 && currentLevel != LEVEL_COMET) {
-        gSPSegment(D_8005BB2C++, D_80031C68_32868[6], (u32)D_8006AA70 & 0x1FFFFFFF);
-    }
-    func_80004C8C_588C();
+	gSPSegment(D_8005BB2C++, 0, 0);
+	gSPSegment(D_8005BB2C++, 1, (u32)__printfunc & 0x1FFFFFFF);
+	gSPSegment(D_8005BB2C++, 5, (u32)D_8006AA6C & 0x1FFFFFFF);
+	gSPSegment(D_8005BB2C++, 0xF, (u32)D_8006AA74 & 0x1FFFFFFF);
+	gSPSegment(D_8005BB2C++, D_80031C68_32868[arg0], (u32)D_8006AA70 & 0x1FFFFFFF);
+	if (D_80052ACA == 2 && currentLevel != LEVEL_COMET) {
+		gSPSegment(D_8005BB2C++, D_80031C68_32868[6], (u32)D_8006AA70 & 0x1FFFFFFF);
+	}
+	func_80004C8C_588C();
 }
 
 s32 func_80011F90_12B90(void *arg0) {
