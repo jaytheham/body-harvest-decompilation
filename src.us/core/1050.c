fcee02c3d430277c32b1ba4cbeaf6825c8773f45
jaytheham <jaytheham@gmail.com>
2026-04-18 10:34:33 +1200

    Improve func_800016D8_22D8 CURRENT(2060)

--- src.us/core/1050.c
+++ src.us/core/1050.c
@@ -529,37 +529,40 @@ void func_800015B4_21B4(s32 arg0, s32 arg1) {
 #ifdef NON_MATCHING
 s32 validateSaveVersionAndChecksum(s32 arg0, s32 arg1)
 {
-  u8 *ptr;
-  u8 version;
-  s32 stored_checksum;
-  s32 computed_checksum;
-  s32 i;
-  ptr = ((u8 *) (&D_800431C0)) + arg0;
-  version = ptr[0];
-  if (version != 0x1C)
-  {
-	osSyncPrintf(&D_8003685C_3745C, version);
-	return 0;
-  }
-  stored_checksum = ((ptr[3] << 8) + ptr[2]) & 0xFFFF;
-  ptr += 4;
-  computed_checksum = 0;
- for (i = 0;i < arg1;i = (i + 1) & 0xFFFF){
-	  computed_checksum = (computed_checksum + (*ptr)) & 0xFFFF;
-	  ptr++;
-  }
-  if (computed_checksum != stored_checksum)
-  {
-	osSyncPrintf(&D_80036870_37470, arg1, stored_checksum);
-	return 0;
-  }
-  return 1;
+	u8 *ptr;
+	u8 version;
+	s32 stored_checksum;
+	s32 computed_checksum;
+	s32 i;
+
+	ptr = ((u8 *)&D_800431C0) + arg0;
+	version = ptr[0];
+	if (version != 0x1C) {
+		osSyncPrintf(&D_8003685C_3745C, version);
+		return 0;
+	}
+
+	stored_checksum = ((ptr[3] << 8) + ptr[2]) & 0xFFFF;
+	ptr += 4;
+
+	computed_checksum = 0;
+	for (i = 0; i < arg1; i = (i + 1) & 0xFFFF) {
+		computed_checksum = (computed_checksum + (*ptr)) & 0xFFFF;
+		ptr++;
+	}
+
+	if (computed_checksum != stored_checksum) {
+		osSyncPrintf(&D_80036870_37470, arg1, stored_checksum);
+		return 0;
+	}
+	return 1;
 }
 
 #else
 #pragma GLOBAL_ASM("asm/nonmatchings/core/1050/validateSaveVersionAndChecksum.s")
 #endif
 
+/* CURRENT(2060) */
 #ifdef NON_MATCHING
 void func_800016D8_22D8(void) {
 	u16 i, writeIdx;
@@ -567,7 +570,7 @@ void func_800016D8_22D8(void) {
 
 	D_800431C8 = (u8)D_800313D0;
 	writeIdx = 9;
-	for (i = 0; i < 5; i++) {
+	for (i = 0; i < 5; i = (i + 1) & 0xFFFF) {
 		u16 j;
 		for (j = 0; j < 6; j++) {
 			(&D_800431C0)[writeIdx++] = *((u8*)(D_80047FB8 + i) + j);
