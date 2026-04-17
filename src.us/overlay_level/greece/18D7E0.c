51e0e8e1ece491bc38d7329085f8bfb3bff8b44b
jaytheham <jaytheham@gmail.com>
2026-04-17 16:56:27 +1200

    Improve func_802D6F7C_18FA8C to CURRENT(4247)

--- src.us/overlay_level/greece/18D7E0.c
+++ src.us/overlay_level/greece/18D7E0.c
@@ -870,7 +870,7 @@ void func_802D6D20_18F830(void) {
 #endif
 
 #ifdef NON_MATCHING
-/* CURRENT(7362) */
+/* CURRENT(4247) */
 void func_802D6F7C_18FA8C(void) {
 	f32 sp5C;
 	s32 pad0;
@@ -878,60 +878,79 @@ void func_802D6F7C_18FA8C(void) {
 	f32 sp58;
 	f32 sp54;
 	f32 sp50;
+	f32 sp4C;
+	f32 sp48;
+	f64 temp_f20;
+	s16 i;
 	s32 temp_a1;
-	u16 sp5A;
-	u16 sp58;
-	u16 sp56;
-	u16 sp54;
 	Unk8013FDA8Entry *sp30;
 	Unk8013FDA8Entry *sp2C;
-	Unk8013FDA8Entry *entry;
 
 	sp5C = (f32) (D_80257DB4 >> 1);
 	sp30 = (Unk8013FDA8Entry *) func_80011F90_12B90(D_9052A00);
 	sp2C = (Unk8013FDA8Entry *) func_80011F90_12B90(D_9052AC0);
 
+	temp_a1 = sins(D_80052B34->unk6);
 	temp_f20 = (f64) sp5C;
-	sp58 = (f32) ((((f64) (f32) sins(D_80052B34->unk6) / 32768.0) * temp_f20) + (f64) D_80052B34->unk4C);
-	sp54 = (f32) ((f64) D_80052B34->unk54 - (((f64) (f32) coss(D_80052B34->unk6) / 32768.0) * temp_f20));
+	sp58 = (f32) ((((f64) (f32) temp_a1 / 32768.0) * temp_f20) + (f64) D_80052B34->unk4C);
+	temp_a1 = coss(D_80052B34->unk6);
+	sp54 = (f32) ((f64) D_80052B34->unk54 - (((f64) (f32) temp_a1 / 32768.0) * temp_f20));
 	sp48 = sqrtf(((D_8004DCAC - sp58) * (D_8004DCAC - sp58)) + ((sp54 - D_8004DCB0) * (sp54 - D_8004DCB0)));
 
 	temp_a1 = (s32) ((f32) (((-(f64) (f32) coss((func_80003824_4424(sp58 - D_8004DCAC, sp54 - D_8004DCB0) - D_80052B34->unk6) & 0xFFFF) / 32768.0) * (f64) -sp48)) * 21.0f);
 	D_8004DCAC = sp58;
 	D_8004DCB0 = sp54;
 
-	entry = (Unk8013FDA8Entry *) ((u8 *) sp30 + 0xB0);
-	for (i = 0xB; i != 0; i--) {
-		entry->unk8.unk0 = (s16) (entry->unk8.unk0 + temp_a1);
-		entry = (Unk8013FDA8Entry *) ((u8 *) entry - 0x10);
+	{
+		Unk8013FDA8Entry *entry;
+
+		entry = (Unk8013FDA8Entry *) ((u8 *) sp30 + 0xB0);
+		for (i = 0xB; i != 0; i--) {
+			entry->unk8.unk0 = (s16) (entry->unk8.unk0 + temp_a1);
+			entry = (Unk8013FDA8Entry *) ((u8 *) entry - 0x10);
+		}
 	}
 
 	temp_a1 = ((s16) sp30[1].unk8.unk0 >> 0xC) << 0xC;
-	entry = (Unk8013FDA8Entry *) ((u8 *) sp30 + 0xB0);
-	for (i = 0xB; i != 0; i--) {
-		entry->unk8.unk0 = (s16) (entry->unk8.unk0 - temp_a1);
-		entry = (Unk8013FDA8Entry *) ((u8 *) entry - 0x10);
+	{
+		Unk8013FDA8Entry *entry;
+
+		entry = (Unk8013FDA8Entry *) ((u8 *) sp30 + 0xB0);
+		for (i = 0xB; i != 0; i--) {
+			entry->unk8.unk0 = (s16) (entry->unk8.unk0 - temp_a1);
+			entry = (Unk8013FDA8Entry *) ((u8 *) entry - 0x10);
+		}
 	}
 
-	sp50 = (f32) ((f64) D_80052B34->unk4C - (((f64) (f32) sins(D_80052B34->unk6) / 32768.0) * temp_f20));
-	sp4C = (f32) ((((f64) (f32) coss(D_80052B34->unk6) / 32768.0) * temp_f20) + (f64) D_80052B34->unk54);
+	temp_a1 = sins(D_80052B34->unk6);
+	sp50 = (f32) ((f64) D_80052B34->unk4C - (((f64) (f32) temp_a1 / 32768.0) * temp_f20));
+	temp_a1 = coss(D_80052B34->unk6);
+	sp4C = (f32) ((((f64) (f32) temp_a1 / 32768.0) * temp_f20) + (f64) D_80052B34->unk54);
 	sp48 = sqrtf(((D_8004DCB4 - sp50) * (D_8004DCB4 - sp50)) + ((sp4C - D_8004DCB8) * (sp4C - D_8004DCB8)));
 
 	temp_a1 = (s32) ((f32) (((-(f64) (f32) coss((func_80003824_4424(sp50 - D_8004DCB4, sp4C - D_8004DCB8) - D_80052B34->unk6) & 0xFFFF) / 32768.0) * (f64) -sp48)) * 21.0f);
 	D_8004DCB4 = sp50;
 	D_8004DCB8 = sp4C;
 
-	entry = (Unk8013FDA8Entry *) ((u8 *) sp2C + 0xB0);
-	for (i = 0xB; i != 0; i--) {
-		entry->unk8.unk0 = (s16) (entry->unk8.unk0 + temp_a1);
-		entry = (Unk8013FDA8Entry *) ((u8 *) entry - 0x10);
+	{
+		Unk8013FDA8Entry *entry;
+
+		entry = (Unk8013FDA8Entry *) ((u8 *) sp2C + 0xB0);
+		for (i = 0xB; i != 0; i--) {
+			entry->unk8.unk0 = (s16) (entry->unk8.unk0 + temp_a1);
+			entry = (Unk8013FDA8Entry *) ((u8 *) entry - 0x10);
+		}
 	}
 
 	temp_a1 = ((s16) sp2C[1].unk8.unk0 >> 0xC) << 0xC;
-	entry = (Unk8013FDA8Entry *) ((u8 *) sp2C + 0xB0);
-	for (i = 0xB; i != 0; i--) {
-		entry->unk8.unk0 = (s16) (entry->unk8.unk0 - temp_a1);
-		entry = (Unk8013FDA8Entry *) ((u8 *) entry - 0x10);
+	{
+		Unk8013FDA8Entry *entry;
+
+		entry = (Unk8013FDA8Entry *) ((u8 *) sp2C + 0xB0);
+		for (i = 0xB; i != 0; i--) {
+			entry->unk8.unk0 = (s16) (entry->unk8.unk0 - temp_a1);
+			entry = (Unk8013FDA8Entry *) ((u8 *) entry - 0x10);
+		}
 	}
 }
 #else
