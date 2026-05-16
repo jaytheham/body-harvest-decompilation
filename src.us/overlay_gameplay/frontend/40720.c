78604d4c95c2c6cc43ccb31243c22751e4bfe52c
jaytheham <jaytheham@gmail.com>
2026-05-17 10:23:09 +1200

    improve

--- src.us/overlay_gameplay/frontend/40720.c
+++ src.us/overlay_gameplay/frontend/40720.c
@@ -765,49 +765,56 @@ void func_80071738_41BE8(void) {
 #pragma GLOBAL_ASM("asm/nonmatchings/overlay_gameplay/frontend/40720/func_80071738_41BE8.s")
 #endif
 
+// https://decomp.me/scratch/DRRoV
+// CURRENT(110)
 #ifdef NON_MATCHING
-// CURRENT(283)
-s32 func_80071760_41C10(s32 arg0) {
+s32 func_80071760_41C10(s32 arg0)
+{
+  s32 totalWidth;
+  s16 digitCount;
+  s16 i;
+  s16 j;
+  s32 value; 
+	s32 divisor;
+	s32 pad;
+	s8 chpad;s8 chpad2;s8 chpad3;s8 ch;
 	s32 textWidth;
-	s32 totalWidth;
-	s16 digitCount;
-	s16 i;
-	s16 j;
-	s32 value;
-	s8 ch;
-
-	digitCount = 0;
-	totalWidth = 0;
-	value = arg0;
-
-	textWidth = func_8000A2B8_AEB8(&D_800AD7E4_7DC94, 0) * 0x1C;
-
-	if (arg0 > 0) {
-		do {
-			digitCount++;
-			value = (s32)((f32)value / 10.0f);
-		} while (value > 0);
+  digitCount = 0;
+  totalWidth = 0;
+  value = arg0;
+  textWidth = func_8000A2B8_AEB8(&D_800AD7E4_7DC94, 0) * 0x1C;
+  if (arg0 > 0)
+  {
+	do
+	{
+	  digitCount++;
+	  value = value / 10.0f;
 	}
+	while (value > 0);
+	  
+  }
+  if (digitCount > 0)
+  {
+	i = 0;
+	do
+	{
+	  divisor = 1;
+	  for (j = 0; j < i; j++)
+	  {
+		divisor *= 10;
+	  }
 
-	if (digitCount > 0) {
-		i = 0;
-		do {
-			s32 divisor = 1;
-			for (j = 0; j < i; j++) {
-				divisor *= 10;
-			}
-
-			value = (s32)((f32)arg0 / (f32)divisor);
-			ch = (value - (s32)((f32)value / 10.0f) * 10) + '0';
-			totalWidth += func_8000A2B8_AEB8(&ch, 0) * 4;
-		} while (++i < digitCount);
+	  value = ((f32) arg0) / divisor;
+	  ch = (value - (((s32) (value / 10.0f)) * 10)) + '0';
+	  totalWidth += func_8000A2B8_AEB8(&ch, 0) * 4;
 	}
-
-	if (arg0 == 0) {
-		totalWidth = func_8000A2B8_AEB8(&D_800AD7E8_7DC98, 0) * 4;
-	}
-
-	return textWidth - totalWidth;
+	while ((++i) < digitCount);
+  }
+  if (arg0 == 0)
+  {
+	totalWidth = func_8000A2B8_AEB8(&D_800AD7E8_7DC98, 0) * 4;
+  }
+  return textWidth - totalWidth;
 }
 #else
 #pragma GLOBAL_ASM("asm/nonmatchings/overlay_gameplay/frontend/40720/func_80071760_41C10.s")
