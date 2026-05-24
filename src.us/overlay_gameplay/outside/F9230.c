cbbcf0a8d84ec40958fc8ec7073ac2a54422c249
jaytheham <jaytheham@gmail.com>
2026-05-25 10:49:48 +1200

    Non-matching decomp of func_8010D4EC_11C49C

--- src.us/overlay_gameplay/outside/F9230.c
+++ src.us/overlay_gameplay/outside/F9230.c
@@ -8355,7 +8355,195 @@ s32 func_8010D234_11C1E4(s16 arg0, s16 arg1) {
 #pragma GLOBAL_ASM("asm/nonmatchings/overlay_gameplay/outside/F9230/func_8010D234_11C1E4.s")
 #endif
 
+// CURRENT(32922)
+#ifdef NON_MATCHING
+s32 func_8010D4EC_11C49C(s16 arg0, s16 arg1, s16 *arg2, s16 *arg3, VehicleInstance *arg4) {
+	f32 xIn;
+	f32 zIn;
+	f32 xArg;
+	f32 zArg;
+	f32 slopeMain;
+	f32 interceptMain;
+	f32 slopeEdge;
+	f32 interceptEdge;
+	f32 x0;
+	f32 z0;
+	f32 x1;
+	f32 z1;
+	f32 x2;
+	f32 z2;
+	f32 xHit;
+	f32 zHit;
+	f32 dx;
+	f32 dz;
+	s32 sideFlags[4];
+	s32 sideA;
+	s32 sideB;
+	s32 startSide;
+	s32 i;
+	s32 outer;
+	s32 closestCorner;
+	s32 farthestCorner;
+	s32 bestDist;
+	s32 worstDist;
+	s32 distSq;
+	s32 chosenCorner;
+	s32 firstCorner;
+	s32 secondCorner;
+	s16 firstAng;
+	s16 secondAng;
+	s16 centerAng;
+
+	func_80003824_4424((f32)(*arg3 - arg1), (f32)(*arg2 - arg0));
+	func_8010C4EC_11B49C(arg4);
+
+	xArg = (f32)arg0;
+	zArg = (f32)arg1;
+	xIn = (f32)*arg2;
+	zIn = (f32)*arg3;
+
+	dx = xArg - xIn;
+	if (xIn > xArg) {
+		dx = -dx;
+	}
+	if (dx < 5.0f) {
+		xIn = xArg + 5.0f;
+	}
+
+	slopeMain = (zIn - zArg) / (xIn - xArg);
+	interceptMain = zArg - (slopeMain * xArg);
+
+	for (i = 3; i >= 0; i--) {
+		z0 = D_80159D5C->unk54 + (&D_80159DA4)[i];
+		x0 = D_80159D5C->unk4C + (&D_80159D84)[i];
+		sideFlags[i] = interceptMain < (z0 - (x0 * slopeMain));
+	}
+
+	bestDist = 0x7FFFFFFF;
+	worstDist = 0;
+	closestCorner = -1;
+	farthestCorner = -1;
+
+	for (outer = 2; outer > 0; outer--) {
+		if ((sideFlags[0] == sideFlags[outer]) || (sideFlags[0] == sideFlags[outer + 1])) {
+			continue;
+		}
+
+		x0 = arg4->unk4C;
+		z0 = arg4->unk54;
+
+		for (i = 3; i >= 0; i--) {
+			dx = ((&D_80159D84)[i] + x0) - xArg;
+			dz = ((&D_80159DA4)[i] + z0) - zArg;
+			distSq = ((s32)dx * (s32)dx) + ((s32)dz * (s32)dz);
+
+			if (distSq < bestDist) {
+				bestDist = distSq;
+				closestCorner = i;
+			}
+
+			if (distSq > worstDist) {
+				worstDist = distSq;
+				farthestCorner = i;
+			}
+		}
+
+		x1 = D_80159D78[closestCorner] + x0;
+		z1 = D_80159D98[closestCorner] + z0;
+
+		slopeEdge = (zArg - z1) / (xArg - x1);
+		interceptEdge = z1 - (slopeEdge * x1);
+		startSide = interceptEdge < (zIn - (xIn * slopeEdge));
+
+		firstCorner = -1;
+		secondCorner = -1;
+		for (i = 3; i >= 0; i--) {
+			if ((i == closestCorner) || (i == farthestCorner)) {
+				continue;
+			}
+
+			x2 = D_80159D78[i] + D_80159D5C->unk4C;
+			z2 = D_80159D98[i] + D_80159D5C->unk54;
+			if (startSide == (interceptEdge < (z2 - (slopeEdge * x2)))) {
+				if (firstCorner == -1) {
+					firstCorner = i;
+					firstAng = func_80003824_4424(z2 - zArg, x2 - xArg);
+				} else {
+					secondCorner = i;
+					secondAng = func_80003824_4424(z2 - zArg, x2 - xArg);
+				}
+			}
+		}
+
+		if (secondCorner == -1) {
+			chosenCorner = firstCorner;
+		} else {
+			centerAng = func_80003824_4424(z1 - zArg, x1 - xArg);
+			firstAng = func_800F9C50_108C00(centerAng, firstAng);
+			secondAng = func_800F9C50_108C00(centerAng, secondAng);
+			chosenCorner = secondCorner;
+			if (((secondAng < 0) ? -secondAng : secondAng) < ((firstAng < 0) ? -firstAng : firstAng)) {
+				chosenCorner = firstCorner;
+			}
+		}
+
+		if (chosenCorner == -1) {
+			*arg2 = -0x7FFF;
+			*arg3 = -0x7FFF;
+			return 0;
+		}
+
+		x0 = arg4->unk4C;
+		z0 = arg4->unk54;
+		x1 = D_80159D78[closestCorner] + x0;
+		z1 = D_80159D98[closestCorner] + z0;
+		xIn = D_80159D78[chosenCorner] + x0;
+		zIn = D_80159D98[chosenCorner] + z0;
+
+		dx = xIn - x1;
+		if (x1 > xIn) {
+			dx = -dx;
+		}
+		if (dx < 5.0f) {
+			xIn = x1 + 5.0f;
+		}
+
+		dz = zIn - z1;
+		if (z1 > zIn) {
+			dz = -dz;
+		}
+		if (dz < 5.0f) {
+			zIn = z1 + 5.0f;
+		}
+
+		slopeEdge = (zIn - z1) / (xIn - x1);
+		interceptEdge = z1 - (slopeEdge * x1);
+
+		x2 = D_80159D5C->unk4C;
+		z2 = D_80159D5C->unk54;
+		sideA = interceptEdge < (z2 - (slopeEdge * x2));
+		sideB = interceptEdge < (zArg - (slopeEdge * xArg));
+
+		xHit = (interceptMain - interceptEdge) / (slopeEdge - slopeMain);
+		zHit = (slopeMain * xHit) + interceptMain;
+
+		if ((D_80144D08_153CB8 < (f64)zHit) || ((f64)zHit < -32768.0) ||
+			(D_80144D10_153CC0 < (f64)xHit) || ((f64)xHit < -32768.0)) {
+			*arg2 = -0x7FFF;
+			*arg3 = -0x7FFF;
+		} else {
+			*arg2 = (s16)(s32)xHit;
+			*arg3 = (s16)(s32)zHit;
+		}
+
+		return sideA != sideB;
+	}
+
+	return 0;
+}
+#else
 #pragma GLOBAL_ASM("asm/nonmatchings/overlay_gameplay/outside/F9230/func_8010D4EC_11C49C.s")
+#endif
 
 // CURRENT(4281)
 #ifdef NON_MATCHING
