4fdecc9e01502368034ea81c4dfb8f5e34297aee
jaytheham <jaytheham@gmail.com>
2026-05-31 10:20:24 +1200

    Improve func_800B2354_C1304 score to 10872

--- src.us/overlay_gameplay/outside/BF9C0.c
+++ src.us/overlay_gameplay/outside/BF9C0.c
@@ -858,113 +858,127 @@ void func_800B1A68_C0A18(s16 *arg0, s16 *arg1, u8 *arg2, Unk80152D00 *arg3) {
 #pragma GLOBAL_ASM("asm/nonmatchings/overlay_gameplay/outside/BF9C0/func_800B1A68_C0A18.s")
 #endif
 
-// CURRENT(11404)
+// CURRENT(10872)
 #ifdef NON_MATCHING
 void func_800B2354_C1304(s16 *arg0, s8 *arg1, s32 arg2, s16 arg3) {
-	s16 baseX;
-	s16 baseY;
-	s16 centerX;
-	s16 centerY;
-	s16 mapOffX;
-	s16 mapOffY;
-	s16 fracX;
-	s16 fracY;
-	s16 radius;
-	s16 start;
-	s16 end;
-	s16 x;
-	s16 y;
-	s16 xDist;
-	s16 yDist;
-	s16 mapX;
-	s16 mapY;
-	s16 dist;
-	s16 falloff;
-	s16 intensity;
+	s32 sp70;
+	s32 sp6C;
+	s32 sp68;
+	s16 sp62;
+	s16 sp60;
+	s32 sp58;
+	s32 sp54;
+	s32 sp50;
+	s32 sp4C;
 	f32 temp_f10;
-	u32 temp_f8;
-	u8 *color;
-	u8 *dst;
+	s16 temp_ra;
+	s16 temp_s3;
+	s16 temp_s4;
+	s16 temp_v0;
+	s16 temp_v0_3;
+	s16 temp_v0_4;
+	s16 temp_v1;
+	s16 temp_v1_3;
+	s16 var_s0;
+	s16 var_s2;
+	s16 var_t2;
+	s16 var_t3;
+	s16 var_v0;
+	s16 var_v0_2;
+	s16 var_v0_3;
+	s16 var_v0_4;
+	s16 var_v0_5;
+	s16 var_v1_2;
+	s32 temp_fp;
+	s32 temp_s5;
+	s32 temp_t5;
+	s32 temp_t7;
+	s32 temp_v0_2;
+	s32 var_a3;
+	u8 *var_a0;
 
-	color = (u8 *)arg1;
-	baseX = (arg0[0] >> 8) + 1;
-	baseY = (arg0[1] >> 8) + 1;
-	fracX = arg0[0] & 0xFF;
-	fracY = arg0[1] & 0xFF;
-	centerX = D_8014F89C - 0x7F;
-	centerY = D_8014F89D - 0x7F;
-	mapOffX = (D_8014F899 + baseX) - centerX;
-	mapOffY = (D_8014F898 + baseY) - centerY;
-	temp_f10 = (f32)(arg2 + arg3);
-	temp_f8 = 0x43800000;
-	temp_f10 = temp_f10 / *(f32 *)&temp_f8;
-	radius = (u8)((u32)temp_f10 + 1);
-	start = 1 - radius;
-	end = radius + 1;
-
-	for (x = start; x < end; x++) {
-		if (x <= 0) {
-			xDist = fracX + (x * -0x100);
-		} else {
-			xDist = ((0x100 - fracX) + (x << 8)) - 0x100;
-		}
-
-		for (y = start; y < end; y++) {
-			if (y <= 0) {
-				yDist = fracY + (y * -0x100);
+	temp_v0 = arg0[0];
+	temp_s4 = D_8014F89D - 0x7F;
+	sp70 = (temp_v0 >> 8) + 1;
+	temp_v1 = arg0[1];
+	temp_s3 = D_8014F89C - 0x7F;
+	sp6C = (temp_v1 >> 8) + 1;
+	var_t2 = (D_8014F899 + sp70) - temp_s3;
+	var_t3 = (D_8014F898 + sp6C) - temp_s4;
+	temp_f10 = (f32)(arg2 + arg3) / 256.0f;
+	sp68 = temp_v0 & 0xFF;
+	temp_s5 = temp_v1 & 0xFF;
+	temp_t5 = (u8)(((u32)temp_f10) + 1);
+	temp_ra = 1 - temp_t5;
+	temp_fp = temp_t5 + 1;
+	var_s2 = temp_ra;
+	sp50 = temp_fp;
+	sp54 = temp_ra;
+	sp58 = temp_ra;
+	if (temp_ra < temp_fp) {
+		var_a3 = temp_fp;
+		do {
+			var_s0 = (s16)((u32)sp58 >> 16);
+			if (var_s2 <= 0) {
+				var_v0 = sp68 + (var_s2 * -0x100);
 			} else {
-				yDist = ((0x100 - fracY) + (y << 8)) - 0x100;
+				var_v0 = ((0x100 - sp68) + (var_s2 << 8)) - 0x100;
 			}
-
-			dist = (s16)((s32)sqrtf((f32)((xDist * xDist) + (yDist * yDist))) - arg2);
-			if (dist > 0) {
-				falloff = arg3 - dist;
-				if (falloff > 0) {
-					intensity = (s16)(((f32)falloff / (f32)arg3) * 255.0f);
-					if (intensity >= 0x100) {
-						intensity = 0xFF;
+			if (sp54 < sp50) {
+				do {
+					if (var_s0 <= 0) {
+						var_v0_2 = temp_s5 + (var_s0 * -0x100);
+					} else {
+						var_v0_2 = ((0x100 - temp_s5) + (var_s0 << 8)) - 0x100;
 					}
-				} else {
-					continue;
-				}
-			} else {
-				intensity = 0xFF;
+					sp4C = var_a3;
+					sp62 = var_t2;
+					sp60 = var_t3;
+					temp_v0_2 = (s32)sqrtf((f32)((var_v0_2 * var_v0_2) + (var_v0 * var_v0))) - arg2;
+					temp_t7 = (s16)temp_v0_2;
+					if ((s16)temp_t7 > 0) {
+						temp_v1_3 = arg3 - (s16)temp_t7;
+						if (temp_v1_3 > 0) {
+							var_v1_2 = (s16)(((f32)temp_v1_3 / (f32)arg3) * 255.0f);
+							if (var_v1_2 >= 0x100) {
+								var_v1_2 = 0xFF;
+							}
+						} else {
+							var_v1_2 = 0;
+						}
+					} else {
+						var_v1_2 = 0xFF;
+					}
+					if (var_v1_2 > 0) {
+						temp_v0_3 = var_s2 + sp70;
+						if (temp_v0_3 >= temp_s3) {
+							temp_v0_4 = var_s0 + sp6C;
+							if (((s16)(temp_s3 + 0x13) >= temp_v0_3) && (temp_v0_4 >= temp_s4) && ((s16)(temp_s4 + 0x13) >= temp_v0_4)) {
+								var_a0 = &D_80152740[(((u16)((s16)((var_s0 + sp60) % 0x13))) * 0x39) + (((u16)((s16)((var_s2 + sp62) % 0x13))) * 3)];
+								var_v0_3 = (s16)(((s32)arg1[0] * var_v1_2) >> 8) + var_a0[0];
+								if (var_v0_3 >= 0x100) {
+									var_v0_3 = 0xFF;
+								}
+								var_a0[0] = (s8)var_v0_3;
+								var_v0_4 = (s16)(((s32)arg1[1] * var_v1_2) >> 8) + var_a0[1];
+								if (var_v0_4 >= 0x100) {
+									var_v0_4 = 0xFF;
+								}
+								var_a0[1] = (s8)var_v0_4;
+								var_v0_5 = (s16)(((s32)arg1[2] * var_v1_2) >> 8) + var_a0[2];
+								if (var_v0_5 >= 0x100) {
+									var_v0_5 = 0xFF;
+								}
+								var_a0[2] = (s8)var_v0_5;
+							}
+						}
+					}
+					var_s0++;
+					var_a3 = sp4C;
+				} while (var_s0 < var_a3);
 			}
-
-			if (intensity <= 0) {
-				continue;
-			}
-
-			mapX = x + baseX;
-			if ((mapX < centerX) || (mapX > (s16)(centerX + 0x13))) {
-				continue;
-			}
-
-			mapY = y + baseY;
-			if ((mapY < centerY) || (mapY > (s16)(centerY + 0x13))) {
-				continue;
-			}
-
-			dst = &D_80152740[((u16)((s16)((y + mapOffY) % 0x13)) * 0x39) + ((u16)((s16)((x + mapOffX) % 0x13)) * 3)];
-
-			dist = ((color[0] * intensity) >> 8) + dst[0];
-			if (dist >= 0x100) {
-				dist = 0xFF;
-			}
-			dst[0] = dist;
-
-			dist = ((color[1] * intensity) >> 8) + dst[1];
-			if (dist >= 0x100) {
-				dist = 0xFF;
-			}
-			dst[1] = dist;
-
-			dist = ((color[2] * intensity) >> 8) + dst[2];
-			if (dist >= 0x100) {
-				dist = 0xFF;
-			}
-			dst[2] = dist;
-		}
+			var_s2++;
+		} while (var_s2 < var_a3);
 	}
 }
 #else
