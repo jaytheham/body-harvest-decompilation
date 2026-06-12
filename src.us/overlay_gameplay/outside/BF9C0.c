b35744b5b71e908a121ae95f9aa8ea1500e9a7f4
jaytheham <jaytheham@gmail.com>
2026-06-12 17:51:56 +1200

    Match

--- src.us/overlay_gameplay/outside/BF9C0.c
+++ src.us/overlay_gameplay/outside/BF9C0.c
@@ -12,21 +12,21 @@ const f64 D_80142E18_151DC8[1] = {127.5};
 const f32 D_80142E20_151DD0[1] = {0.5759139060974121f};
 const f32 D_80142E24_151DD4[1] = {0.5759139060974121f};
 const u32 jtbl_80142E28_151DD8[] = {
-    0x800B58D4,
-    0x800B59C0,
-    0x800B5AC8,
-    0x800B5BB4,
-    0x800B5CA0,
+	0x800B58D4,
+	0x800B59C0,
+	0x800B5AC8,
+	0x800B5BB4,
+	0x800B5CA0,
 };
 const f64 D_80142E40_151DF0[1] = {1.022};
 const f32 D_80142E48_151DF8[1] = {0.699999988079071f};
 const f32 D_80142E4C_151DFC[1] = {3.200000047683716f};
 const u32 jtbl_80142E50_151E00[] = {
-    0x800B9784,
-    0x800B97D4,
-    0x800B981C,
-    0x800B9864,
-    0x800B98AC,
+	0x800B9784,
+	0x800B97D4,
+	0x800B981C,
+	0x800B9864,
+	0x800B98AC,
 };
 const f64 D_80142E68_151E18[1] = {0.4};
 const f64 D_80142E70_151E20[1] = {0.2};
@@ -106,71 +106,42 @@ s32 func_800B0A10_BF9C0(s32 arg0, s32 arg1, s16 arg2, s16 arg3) {
 	return (s32)(((arg3 - arg2) * arg0) + (arg1 * arg2)) / arg3;
 }
 
-// CURRENT(160)
-#ifdef NON_MATCHING
-s32 func_800B0A88_BFA38(s32 arg0, s32 arg1) {
-	s32 a0;
-	s32 a1;
-	s32 a2;
-	s32 a3;
-	s32 v0;
-	s32 t0;
+s32 func_800B0A88_BFA38(s32 arg0, s32 arg1)
+{
+  s32 a0;
+  s32 v0;
+  v0 = (D_80052A94[arg1].unk0[arg0] & 0x3F) - (D_80052A94[arg1].unk0[arg0 + 1] & 0x3F);
+  a0 = (-v0 < v0) ? v0 : -v0;
+  
+  if (a0 >= 0xA)
+  {
+	return 1;
+  }
 
-	a2 = D_80052A94[arg1].unk0[arg0] & 0x3F,
-	a3 = D_80052A94[arg1].unk0[arg0 + 1] & 0x3F,
-	v0 = a2 - a3,
-	t0 = -v0;
-	if (t0 < v0) {
-		a0 = v0;
-	} else {
-		a0 = t0;
-	}
-	if (a0 >= 0xA) {
-		return 1;
-	}
+  v0 = (D_80052A94[arg1].unk0[arg0 + 1] & 0x3F) - (D_80052A94[arg1].unk0[arg0 + 0x101] & 0x3F);
+  a0 = (-v0 < v0) ? v0 : -v0;
+  
+  if (a0 >= 0xA)
+  {
+	return 1;
+  }
 
-	a1 = D_80052A94[arg1].unk0[arg0 + 0x101] & 0x3F,
-	v0 = a3 - a1,
-	t0 = -v0;
-	if (t0 < v0) {
-		a0 = v0;
-	} else {
-		a0 = t0;
-	}
-	if (a0 >= 0xA) {
-		return 1;
-	}
-
-	a3 = D_80052A94[arg1].unk0[arg0 + 0x100] & 0x3F,
-	v0 = a1 - a3,
-	t0 = -v0;
-	if (t0 < v0) {
-		a0 = v0;
-	} else {
-		a0 = t0;
-	}
-	if (a0 >= 0xA) {
-		return 1;
-	}
-
-	v0 = a3 - a2,
-	t0 = -v0;
-	if (t0 < v0) {
-		a0 = v0;
-	} else {
-		a0 = t0;
-	}
-	if (a0 >= 0xA) {
-		return 1;
-	}
-
-	return 0;
-
-	return 0;
+  v0 = (D_80052A94[arg1].unk0[arg0 + 0x101] & 0x3F) - (D_80052A94[arg1].unk0[arg0 + 0x100] & 0x3F);
+  a0 = (-v0 < v0) ? v0 : -v0;
+  
+  if (a0 >= 0xA)
+  {
+	return 1;
+  }
+  v0 = (D_80052A94[arg1].unk0[arg0 + 0x100] & 0x3F) - (D_80052A94[arg1].unk0[arg0] & 0x3F);
+  a0 = (-v0 < v0) ? v0 : -v0;
+  
+  if (a0 >= 0xA)
+  {
+	return 1;
+  }
+  return 0;
 }
-#else
-#pragma GLOBAL_ASM("asm/nonmatchings/overlay_gameplay/outside/BF9C0/func_800B0A88_BFA38.s")
-#endif
 
 void func_800B0B94_BFB44(void) {
 	u8 *s2;
