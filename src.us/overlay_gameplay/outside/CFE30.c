cfdf1bfa13357e8aa645306cfd8242b413378cb6
jaytheham <jaytheham@gmail.com>
2026-06-15 17:56:23 +1200

    Match

--- src.us/overlay_gameplay/outside/CFE30.c
+++ src.us/overlay_gameplay/outside/CFE30.c
@@ -8791,6 +8791,7 @@ void func_800D9B14_E8AC4(s16 arg0, s16 arg1, s16 arg2, s16 arg3) {
 	temp_v0->unk12 = arg2;
 }
 
+// CURRENT(553)
 #ifdef NON_MATCHING
 void func_800D9B54_E8B04(void) {
 	s16 temp_s2;
@@ -8830,22 +8831,20 @@ void func_800D9C60_E8C10(s16 arg0) {
 	}
 }
 
-#ifdef NON_MATCHING
-void func_800D9C98_E8C48(s16 arg0, s16 arg1) {
+void func_800D9C98_E8C48(s16 arg0, s16 arg1)
+{
 	s32 sp2C;
-	s16 *entryData = (s16 *)&D_80154318[arg0].unk8;
-	if (entryData[2] & 1) {
+	s16 *entryData = (s16 *)(&D_80154318[arg0].unk8);
+	if (entryData[2] & 1)
+	{
 		s32 rnd;
-		sp2C = func_800DEE5C_EDE0C(entryData[3], (s16)(s32)(entryData[4] + ((f64)(f32)sins(0x4000 - (entryData[2] << 10)) / 32768.0) * D_80144028_152FD8[0]), entryData[5], 0x46, 0x18 - entryData[2]);
+		sp2C = func_800DEE5C_EDE0C(entryData[3], (s16)((s32)(entryData[4] + (D_80144028_152FD8[0] * (((f64)((f32)sins(0x4000 - (entryData[2] << 10)))) / 32768.0)))), entryData[5], 0x46, 0x18 - entryData[2]);
 		rnd = func_800038E0_44E0() % 128;
 		rnd += 0x40;
 		func_800DDD90_ECD40((u8)sp2C, 0xFF, rnd, 0);
 		func_80135D44_144CF4(entryData[3], entryData[4], entryData[5], (f32)(entryData[2] >> 1));
 	}
 }
-#else
-#pragma GLOBAL_ASM("asm/nonmatchings/overlay_gameplay/outside/CFE30/func_800D9C98_E8C48.s")
-#endif
 
 void func_800D9DD8_E8D88(s16 arg0, s16 arg1, s16 arg2) {
 	if (func_800D99F4_E89A4(&func_800D9C98_E8C48, 0x10, arg0, arg1, (s32) arg2) != -3) {
