54149e7ae9b535dbf187745d3a316b4ef0d1646a
jaytheham <jaytheham@gmail.com>
2026-07-24 18:29:49 +1200

    imp

--- src.us/overlay_gameplay/outside/884C0.c
+++ src.us/overlay_gameplay/outside/884C0.c
@@ -7660,12 +7660,13 @@ void func_8008B8E4_9A894(u8 arg0)
 	}
 }
 
-// CURRENT(2386)
+// CURRENT(1928)
 #ifdef NON_MATCHING
 void func_8008BC58_9AC08(u8 arg0)
 {
 	AlienInstance *inst;
-	s32 timer;
+	s32 pad0;
+	s32 pad1;
 	s32 sp70;
 	s32 sp6C;
 	s32 sp68;
@@ -7677,14 +7678,12 @@ void func_8008BC58_9AC08(u8 arg0)
 		return;
 	}
 
-	timer = inst->unk2C;
-	if (timer == 0x28)
+	if (inst->unk2C == 0x28)
 	{
 		func_800D6ADC_E5A8C(inst->unk0, inst->unk2, inst->unk4, 5);
-		timer = inst->unk2C;
 	}
 
-	if (timer < 0x28)
+	if (inst->unk2C < 0x28)
 	{
 		u32 divisor = (func_800038E0_44E0() % 5) + 2;
 		if (((u32)D_80052A8C % divisor) == 0)
@@ -7705,45 +7704,39 @@ void func_8008BC58_9AC08(u8 arg0)
 				(s16)((randB % 0x2BC) + inst->unk2),
 				(s16)(((randC % 0x190) + inst->unk4) - 0xC8),
 				(u16)((randD % 0x64) + 0x28),
-				timer % 4);
+				inst->unk2C % 4);
 		}
 	}
 
-	timer = inst->unk2C;
-	if ((timer < 0x50) && (timer >= 0x10))
+	if ((inst->unk2C < 0x50) && (inst->unk2C >= 0x10))
 	{
-		VehicleInstance *vehicle;
-		AlienInstance *otherAlien;
 		s32 idx;
 		s32 iter;
 
 		func_80128504_1374B4(inst, 0, &sp70, &sp6C, &sp68);
 
-		idx = timer - 0x10;
-		vehicle = &vehicleInstances[idx];
-		if ((D_80052B34 != vehicle) && (vehicle->unk1A != 0))
+		idx = inst->unk2C - 0x10;
 		{
-			if (func_80084E54_93E04(inst, vehicle) < 0x2000)
+			VehicleInstance *vehicle = &vehicleInstances[idx];
+			if ((D_80052B34 != vehicle) && (vehicle->unk1A != 0))
 			{
+				if (func_80084E54_93E04(inst, vehicle) < 0x2000)
+				{
 					func_80123E90_132E40(vehicle, 0x7FFF);
+				}
 			}
 		}
-
-		iter = 0;
-		while (iter != 0x100)
+		for (iter = 0; iter != 0x100; iter += 0x40, idx += 0x40)
 		{
 			if ((idx != 0xFF) && (idx >= 0) && (idx != arg0))
 			{
-				otherAlien = &alienInstances[idx];
+				AlienInstance *otherAlien = &alienInstances[idx];
 				if (otherAlien->specIndex != 0)
 				{
 					func_80079910_888C0(idx);
 					func_800DF848_EE7F8(otherAlien->unk0, otherAlien->unk2, otherAlien->unk4, (u16)alienSpecs[otherAlien->specIndex].unkC, 0);
 				}
 			}
-
-			iter += 0x40;
-			idx += 0x40;
 		}
 	}
 }
