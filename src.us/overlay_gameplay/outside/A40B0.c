b96a8c89b5f965e27b0a0da2ad8f6bb45cc7dfa2
jaytheham <jaytheham@gmail.com>
2026-07-27 19:28:14 +1200

    imp

--- src.us/overlay_gameplay/outside/A40B0.c
+++ src.us/overlay_gameplay/outside/A40B0.c
@@ -434,24 +434,20 @@ void func_80095530_A44E0(s16 arg0) {
 #pragma GLOBAL_ASM("asm/nonmatchings/overlay_gameplay/outside/A40B0/func_80095530_A44E0.s")
 #endif
 
-// Debug - display menu items
+// CURRENT(354)
 #ifdef NON_MATCHING
-void func_8009594C_A48FC(s16 arg0, s16 arg1) {
-	s32 var_s0;
-	s32 var_s1;
-	s32 temp_s2;
-
-	var_s0 = 0;
-	if (arg1 > 0) {
-		var_s1 = arg0;
-		do {
-			temp_s2 = var_s0 + 1;
-			func_80095100_A40B0(var_s1, temp_s2);
-			var_s0 = temp_s2;
-			var_s1 += 1;
-		} while (temp_s2 != arg1);
-	}
-	D_8014ECF4 = arg1;
+// Debug - display menu items
+void func_8009594C_A48FC(s16 arg0, s16 arg1)
+{
+  s32 var_s0;
+  s32 var_s1;
+  s32 temp_s2;
+  
+  for (var_s0 = 0, var_s1 = arg0; arg1 > var_s0; var_s0++, var_s1++)
+  {
+      func_80095100_A40B0(var_s1, var_s0 + 1);
+  }
+  D_8014ECF4 = arg1;
 }
 #else
 #pragma GLOBAL_ASM("asm/nonmatchings/overlay_gameplay/outside/A40B0/func_8009594C_A48FC.s")
