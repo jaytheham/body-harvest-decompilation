425e4f7ff5c851bcdfd156ddab436d4807cbfabd
jaytheham <jaytheham@gmail.com>
2026-08-23 15:24:54 +1200

    comet data

--- src.us/overlay_level/comet/318E20.h
+++ src.us/overlay_level/comet/318E20.h
@@ -39,4 +39,45 @@ void func_802E3584_3276D4(s32 arg0, s32 arg1, s32 arg2, s32 arg3);
 void func_802E43FC_32854C(u8 arg0, s16 arg1);
 void func_802E46B8_328808(u8 arg0, s32 arg1, s32 arg2);
 
+/* Alien type handlers defined in the comet level overlay (318E20.c). */
+void func_802D76B0_31B800(u8 arg0);
+void func_802D7B4C_31BC9C(u8 arg0);
+void func_802D7C1C_31BD6C(u8 arg0);
+void func_802D7F10_31C060(u8 arg0);
+void func_802D897C_31CACC(u8 arg0);
+void func_802D89F0_31CB40(u8 arg0);
+void func_802D8B18_31CC68(u8 arg0);
+void func_802D9100_31D250(u8 arg0);
+void func_802D9128_31D278(u8 arg0);
+void func_802D93D8_31D528(s32 arg0);
+void func_802D95F8_31D748(s32 arg0);
+void func_802D9658_31D7A8(u8 arg0);
+void func_802D9F60_31E0B0(u8 arg0);
+void func_802DA6A4_31E7F4(u8 arg0);
+void func_802DA6D4_31E824(s32 arg0);
+void func_802DAD78_31EEC8(u8 arg0);
+void func_802DB1D4_31F324(s32 arg0);
+void func_802DB9FC_31FB4C(u8 arg0);
+void func_802DBD18_31FE68(u8 arg0);
+void func_802DBDD0_31FF20(u8 arg0);
+void func_802DBF88_3200D8(u8 arg0);
+void func_802DC2A4_3203F4(u8 arg0);
+void func_802DC2D4_320424(u8 arg0);
+void func_802DC8F4_320A44(u8 arg0);
+void func_802DD748_321898(s32 arg0);
+void func_802DE0F4_322244(u8 arg0);
+void func_802DE7C4_322914(u8 arg0);
+void func_802E2B78_326CC8(u8 arg0);
+void func_802E33B0_327500(u8 arg0);
+void func_802E3744_327894(s32 arg0);
+void func_802E3808_327958(s32 arg0);
+void func_802E38D8_327A28(u8 arg0);
+void func_802E3A4C_327B9C(s32 arg0);
+void func_802E3E64_327FB4(u8 arg0);
+void func_802E4214_328364(u8 arg0);
+void func_802E4774_3288C4(u8 arg0);
+void func_802E4918_328A68(u8 arg0);
+void func_802E4ACC_328C1C(u8 arg0);
+void func_802E4CB4_328E04(u8 arg0);
+
 #endif
